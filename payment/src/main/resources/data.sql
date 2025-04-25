a454da3c3f79129ae3f9ddae573d3f9a20f30966
sadopwnz <sadopwnz@yandex.ru>
2025-04-25 20:56:04 +0300

    user balance update

--- payment/src/main/resources/data.sql
+++ payment/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO payment_account (user_id, amount) VALUES (1, 100000.00) ON CONFLICT (user_id) DO NOTHING;
+INSERT INTO payment_account (user_id, amount) VALUES (1, 2000.00) ON CONFLICT (user_id) DO NOTHING;
